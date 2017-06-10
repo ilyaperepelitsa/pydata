38cee0a202f557b49ef0c76d8d42f07617884033
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2017-06-09 23:34:01 -0400

    pew pew

--- practice.py
+++ practice.py
@@ -96,3 +96,84 @@ arr
 arr2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 arr2d[2]
 arr2d[0][2]
+
+
+arr2d[:2]
+arr2d[:2, 1:]
+
+
+
+#### boolean indexing
+
+names = np.array(["Bob", "Joe", "Will", "Bob", "Will", "Joe", "Joe"])
+data = np.random.randn(7, 4)
+# data
+
+names
+
+names == "Bob"
+data[names == "Bob"]
+data[names == "Bob", 2:]
+data[names == "Bob", 2]
+
+
+data[names != "Bob"]
+data[~(names == "Bob")]
+
+
+(names == "Bob") | (names == "Will")
+
+data[(names == "Bob") | (names == "Will")]
+data[data < 0] = 0
+data
+
+
+data[names != "Joe"] = 7
+data
+
+
+arr = np.empty((8, 4))
+arr
+for i in range(8):
+    arr[i] = i
+
+arr
+
+arr[[4, 3, 0, 6]]
+
+
+arr = np.arange(32).reshape((8, 4))
+arr
+arr[[1, 5], [0, 3]]
+arr[[1, 5, 7, 2], [0, 3, 1, 2]]
+arr[[1, 5]]
+
+
+arr[[1, 5, 7, 2]][:, [0, 3, 1, 2]]
+
+
+arr[np.ix_([1, 5, 7, 2], [0, 3, 1, 2])]
+
+
+
+
+### transposing arrays
+
+arr = np.arange(15).reshape((3, 5))
+arr
+
+arr.T
+
+
+
+arr = np.random.randn(6, 3)
+arr
+
+
+np.dot(arr.T, arr)
+
+
+arr = np.arange(16).reshape((2, 2, 4))
+arr
+arr.swapaxes(1, 2)
+'63-3322-5946-0501-9'.replace("-", "")
